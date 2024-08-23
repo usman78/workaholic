4055dae56d31f721c1552c6a4f00514a0073f5bf
usman78 <iam.usmanali1@gmail.com>
2024-08-23 11:13:12 +0500

    template settings

--- .venv/workaholic/members/views.py
+++ .venv/workaholic/members/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
+
+
+def login_user(request):
+    return render(request, 'authenticate/login.html', {})
